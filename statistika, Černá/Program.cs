1e32082cb1ea78a537138a56b1535adf452fd84f
Romana Černá <Cerna.Romana@stud.vassboskovice.cz>
2021-11-23 08:39:17 +0100

    Přidejte soubory projektu.

---
+++ statistika, Černá/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace statistika__Černá
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
